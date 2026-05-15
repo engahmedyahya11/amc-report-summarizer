6551cd47ce68c0ded79a2b52b917e19d187d19fd
engahmedyahya11 <ahmed.yahya.saad11@gmail.com>
2026-05-15 10:49:11 +0300

    Simplify demo.launch by removing parameters
    Removed server configuration parameters from demo.launch.

--- app.py
+++ app.py
@@ -494,9 +494,4 @@ with gr.Blocks() as demo:
     </div>
     """)
 
-demo.launch(
-    server_name="0.0.0.0",
-    server_port=int(os.environ.get("PORT", 7860)),
-    css=css,
-    theme=gr.themes.Base()
-)
+demo.launch()
